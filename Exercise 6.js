6b6165bc1c556201c6cd75cf11cbbbc3679c4715
rehlinger <rehlinger@outlook.com>
2017-08-03 22:49:15 -0400

    Exercise 6 - first

--- Exercise 6.js
+++ Exercise 6.js
@@ -1,1 +1,28 @@
-test = "hello"
+//Create a smarter plant eater that reprouces slower and allows the plants to never leave.
+//Create a target property of the plant eater, slightly reduced intial reproduction energy.
+function SmartPlantEater() {
+    this.energy = 30;
+    this.target = "n";
+}
+
+//Using standard plant eater act method with the modification that
+//planteaters are allowed to eat only if the plant it is targeting has more than 1 nearby plant
+//planteaters also require more energy to reproduce
+SmartPlantEater.prototype.act = function(view) {
+    var space = view.find(" ");
+    if (this.energy > 90 && space)
+      return {type: "reproduce", direction: space};
+    var plant = view.find("*");
+    var cluster = false;
+    if (plant) {
+        this.target = view.vector.plus(directions[plant]);
+        let plantView = new View(view.world, this.target)
+        if (plantView.findAll("*").length <= 2) {
+            cluster = true;
+        }
+    }
+    if (plant && !cluster)
+      return {type: "eat", direction: plant};
+    if (space || cluster)
+      return {type: "move", direction: space};
+}
